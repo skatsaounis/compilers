a6e3a3884fc4c2da4a5801bea99c2e03ec3d7f6a
skatsaounis <katsaouniss@gmail.com>
2016-09-20 10:17:12 +0300

    Clean Up quads.h and generator.h code

--- generator.h
+++ generator.h
@@ -1,8 +1,8 @@
 #ifndef __GENERATOR_H__
 #define __GENERATOR_H__
 
-typedef struct{
-	int id;
+typedef struct {
+	int    id;
 	char * quad;
 	char * arg1;
 	char * arg2;
@@ -12,36 +12,36 @@ typedef struct{
 	char * arg2_pm, * arg2_type, * arg2_nesting, * arg2_kind, * arg2_offset, * arg2_prev_param_string;
 	char * dest_pm, * dest_type, * dest_nesting, * dest_kind, * dest_offset, * dest_prev_param_string;
 	char * next_words;
-}Interpreted_quad;
+} Interpreted_quad;
 
-typedef struct program_strings_struct{
-	int id;
+typedef struct program_strings_struct {
+	int    id;
 	char * node_str;
 	struct program_strings_struct * next;
-}program_string_t;
+} program_string_t;
 
 typedef program_string_t * Program_strings;
 
-void generator(int * externs, int * offsets, FILE * fp, FILE * fp2, FILE * fp3);
-Interpreted_quad consume_quad(FILE * fp);
-void print_consumed_quad(Interpreted_quad quad);
-void generate(Interpreted_quad quad, FILE * fp, int offset);
-void load(char * a, char * b, FILE * fp, char * data_pm, char * data_type, char * data_nesting, char * nesting, char * data_kind, char * data_offet);
-void store(char * a, char * b, FILE * fp, char * data_pm, char * data_type, char * data_nesting, char * nesting, char * data_kind, char * data_offset);
-void loadAddr(char * a, char * b, FILE * fp, char * data_pm, char * data_type, char * data_nesting, char * nesting, char * data_kind, char * data_offset);
-void updateAL(FILE * fp, char * a, char * nesting, int is_lib_function);
-void getAR(char * a, FILE * fp, char * nesting);
-char * endof(char * a);
-char * label(char * a);
-char * name(char * a);
-void printstrings(FILE * fp);
-void printexterns(FILE * fp, int * externs);
-void printexterns2(FILE * fp, int * externs);
-void string_to_db(FILE * fp, char * node_str);
-void print_call_table(FILE * fp, char * fun_name, int call_counter, int temp_var_offset, int * param_byte_table, int * inception_function_table, char * next_words, char ** prev_param_offset_table);
-void comment(FILE *fp, FILE *fp2);
-int is_lib_function(char * function_name);
+void             generator           (int * externs, int * offsets, FILE * fp, FILE * fp2, FILE * fp3);
+Interpreted_quad consume_quad        (FILE * fp);
+void             print_consumed_quad (Interpreted_quad quad);
+void             generate            (Interpreted_quad quad, FILE * fp, int offset);
+void             load                (char * a, char * b, FILE * fp, char * data_pm, char * data_type, char * data_nesting, char * nesting, char * data_kind, char * data_offet);
+void             store               (char * a, char * b, FILE * fp, char * data_pm, char * data_type, char * data_nesting, char * nesting, char * data_kind, char * data_offset);
+void             loadAddr            (char * a, char * b, FILE * fp, char * data_pm, char * data_type, char * data_nesting, char * nesting, char * data_kind, char * data_offset);
+void             updateAL            (FILE * fp, char * a, char * nesting, int is_lib_function);
+void             getAR               (char * a, FILE * fp, char * nesting);
+char *           endof               (char * a);
+char *           label               (char * a);
+char *           name                (char * a);
+void             printstrings        (FILE * fp);
+void             printexterns        (FILE * fp, int * externs);
+void             printexterns2       (FILE * fp, int * externs);
+void             string_to_db        (FILE * fp, char * node_str);
+void             print_call_table    (FILE * fp, char * fun_name, int call_counter, int temp_var_offset, int * param_byte_table, int * inception_function_table, char * next_words, char ** prev_param_offset_table);
+void             comment             (FILE * fp, FILE * fp2);
+int              is_lib_function     (char * function_name);
 
-extern int ProduceFinal;
+extern int       ProduceFinal;
 
 #endif
--- quads.h
+++ quads.h
@@ -4,87 +4,86 @@
 #include "symbol.h"
 #define MAX_QUAD_NUM 1000
 
-typedef enum {               /***********/
-	PLUS_QUAD,               /*    +    */
-	MINUS_QUAD,              /*    -    */
-	MULT_QUAD,               /*    *    */
-	DIV_QUAD,                /*    /    */
-	MOD_QUAD,                /*    %    */
-	EQ_QUAD,                 /*    =    */
-	NE_QUAD,                 /*    <>   */
-	GT_QUAD,                 /*    >    */
-	LT_QUAD,                 /*    <    */
-	GE_QUAD,                 /*    >=   */
-	LE_QUAD,                 /*    <=   */
-	ASSIGN_QUAD,             /*    :=   */
-	JMP_QUAD,                /*   jump  */
-	RET_QUAD,                /*   ret   */
-    RETV_QUAD,               /*   retv  */
-	ARRAY_QUAD,              /*   array */
-    ISNIL_QUAD,              /*   nil?  */
-	UNIT_QUAD,               /*   unit  */
-	ENDU_QUAD,               /*   endu  */
-	PAR_QUAD,                /*   par   */
-	CALL_QUAD,               /*   call  */
-	IFB_QUAD,                /*   ifb   */
-	PTR_QUAD                /* ^points */
-} QuadType;                  /***********/
+typedef enum {                   /***********/
+    PLUS_QUAD,                   /*    +    */
+    MINUS_QUAD,                  /*    -    */
+    MULT_QUAD,                   /*    *    */
+    DIV_QUAD,                    /*    /    */
+    MOD_QUAD,                    /*    %    */
+    EQ_QUAD,                     /*    =    */
+    NE_QUAD,                     /*    <>   */
+    GT_QUAD,                     /*    >    */
+    LT_QUAD,                     /*    <    */
+    GE_QUAD,                     /*    >=   */
+    LE_QUAD,                     /*    <=   */
+    ASSIGN_QUAD,                 /*    :=   */
+    JMP_QUAD,                    /*   jump  */
+    RET_QUAD,                    /*   ret   */
+    RETV_QUAD,                   /*   retv  */
+    ARRAY_QUAD,                  /*   array */
+    ISNIL_QUAD,                  /*   nil?  */
+    UNIT_QUAD,                   /*   unit  */
+    ENDU_QUAD,                   /*   endu  */
+    PAR_QUAD,                    /*   par   */
+    CALL_QUAD,                   /*   call  */
+    IFB_QUAD,                    /*   ifb   */
+    PTR_QUAD                     /* ^points */
+} QuadType;                      /***********/
 
-typedef struct
-{
-	char * pm;
-	char * type;
-	char * nesting;
-	char * kind;
-	char * offset;
-	char * prev_param_string;
+typedef struct {
+    char * pm;
+    char * type;
+    char * nesting;
+    char * kind;
+    char * offset;
+    char * prev_param_string;
 } Quad_req;
 
 typedef struct {                 /*********/
-	QuadType type;           /*  op   */
-	char *arg1, *arg2;       /* x, y  */
-	char *dest;              /*   z   */
-	Quad_req arg1_req, arg2_req;
-	Quad_req dest_req;
+    QuadType type;               /*  op   */
+    char * arg1, * arg2;         /* x, y  */
+    char * dest;                 /*   z   */
+    Quad_req arg1_req, arg2_req;
+    Quad_req dest_req;
 } Quad;                          /*********/
 
 typedef struct label_list_struct {
-	long label;
-	struct label_list_struct * next;
+    long label;
+    struct label_list_struct * next;
 } label_list_t;
 
 typedef label_list_t * label_list;
 
-/* -------------------------------------------------------------------------------------------------------
-   ------------------------ Οι συναρτήσεις μας -----------------------------------------------------------
-   ------------------------------------------------------------------------------------------------------- */
+/* -----------------------------------------------------------------------------------------------------------------------------------
+   ------------------------------------------------------ Custom Functions -----------------------------------------------------------
+   ----------------------------------------------------------------------------------------------------------------------------------- */
 
-extern Quad quad_array[MAX_QUAD_NUM];            /* an array storing quads                                 */
+extern Quad quad_array[MAX_QUAD_NUM];                                        /* an array storing quads                                 */
 extern char units[256][256];
 extern char unit_flags[256];
-extern int unit_counter;
-extern long nextquad;                            /* returns next's quad label                              */
-extern int ProduceInterm;
+extern int  unit_counter;
+extern long nextquad;                                                        /* returns next's quad label                              */
+extern int  ProduceInterm;
 
-long GenQuad(QuadType q, SymbolEntry * x,        /* generates next quad                                    */
-	SymbolEntry * y, SymbolEntry * z, int offset, char * prev_param_string);
-label_list emptylist();                          /* generates an empty quad labels' list                   */
-label_list make_list(long val);                  /* generates quad label's list containing label: val      */
-label_list merge(label_list a, label_list b);    /* merges two quad labels' lists                          */
-void backpatch(label_list list, long val);       /* replace unknown labels of quads of list l with label z */
+label_list  emptylist          ();                                           /* generates an empty quad labels' list                   */
+label_list  make_list          (long val);                                   /* generates quad label's list containing label: val      */
+label_list  merge              (label_list a, label_list b);                 /* merges two quad labels' lists                          */
+void        backpatch          (label_list list, long val);                  /* replace unknown labels of quads of list l with label z */
 
-char * print_quad(int i);                        /* printable version of quad type                         */
-/*void FreeQuads(void);                             Should we use it ?!? */
-char *strdup(const char *str);
-void print_all_quads(FILE *fp, FILE *fp2);                          /* prints all quad_array                                  */
-char * outp(char *inp);
-long GenQuad2(QuadType q, SymbolEntry * x, SymbolEntry * y, char * z, int offset, char * prev_param_string);
-long GenQuad3(QuadType q, char * x, char * y, char * z);
-long GenQuad4(QuadType q, SymbolEntry * x, char * y, char * z);
-char * symbol_type (SymbolEntry * p);
-char * symbol_pm (SymbolEntry * p);
-char * symbol_kind (SymbolEntry * p);
-char * symbol_offset (SymbolEntry * p);
-Type lookup_type_find_b(SymbolEntry * p);
-long dest_replace(SymbolEntry *z);
+char *      print_quad         (int i);                                      /* printable version of quad type                         */
+char *      strdup             (const char * str);
+void        print_all_quads    (FILE * fp, FILE * fp2);                      /* prints all quad_array                                  */
+char *      outp               (char * inp);
+char *      symbol_type        (SymbolEntry * p);
+char *      symbol_pm          (SymbolEntry * p);
+char *      symbol_kind        (SymbolEntry * p);
+char *      symbol_offset      (SymbolEntry * p);
+Type        lookup_type_find_b (SymbolEntry * p);
+long        dest_replace       (SymbolEntry *z);
+long        GenQuad            (QuadType q, SymbolEntry * x, SymbolEntry * y, SymbolEntry * z, int offset, char * prev_param_string);
+long        GenQuad2           (QuadType q, SymbolEntry * x, SymbolEntry * y, char * z, int offset, char * prev_param_string);
+long        GenQuad3           (QuadType q, char * x, char * y, char * z);
+long        GenQuad4           (QuadType q, SymbolEntry * x, char * y, char * z);
+
+
 #endif
